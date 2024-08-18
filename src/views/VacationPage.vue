a8d7515a59825669dc261edb684e03c3d1847ede
Josquin2 <Josquinwork@mail.ru>
2024-08-19 02:49:09 +0400

    added vacation page

---
+++ src/views/VacationPage.vue
@@ -0,0 +1,18 @@
+<script setup lang="ts">
+import { ref } from 'vue'
+import Companies from '@/components/blocks/Companies.vue'
+import DocumentsPage from './DocumentsPage.vue'
+
+const company = ref('Скай Альянс')
+
+const handleCompanyNameUpdate = (newName: string) => {
+  company.value = newName // Обновляем значение переменной company
+}
+</script>
+
+<template>
+  <div class="regulatory-documents">
+    <Companies @update-company-name="handleCompanyNameUpdate" />
+    <DocumentsPage endpoint="vacations" title="Заявления на отпуск" :params="company" />
+  </div>
+</template>
