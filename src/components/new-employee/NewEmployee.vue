d459fdc699d66b071fd044896f17816ac3c1ebac
Josquin2 <Josquinwork@mail.ru>
2024-08-31 21:44:30 +0400

    added permissions

--- src/components/new-employee/NewEmployee.vue
+++ src/components/new-employee/NewEmployee.vue
@@ -1,10 +1,37 @@
 <script setup lang="ts">
+import { ref, onMounted } from 'vue'
 import DownloadBlock from '@/components/blocks/DownloadBlock.vue'
 import router from '@/router'
+import { useRoute } from 'vue-router'
+import { toast } from 'vue3-toastify'
+import 'vue3-toastify/dist/index.css'
+
+const route = useRoute()
 
 function onCreateNewEmployeeClick() {
-  router.push({ name: 'create-new-employee' })
+  const login = route.params.login
+
+  if (canCreate.value == 1) {
+    router.push({ name: 'create-new-employee', params: { login: login } })
+  } else {
+    toast('Вы не можете создать пользователя!', { position: toast.POSITION.BOTTOM_RIGHT })
+  }
 }
+
+const canCreate = ref(0)
+
+onMounted(() => {
+  if (localStorage.getItem('permissions')) {
+    const permissions = JSON.parse(localStorage.getItem('permissions') || '')
+    if (permissions === null) {
+      canCreate.value = 0
+    } else {
+      if (permissions['users.create']) {
+        canCreate.value = permissions['users.create']
+      }
+    }
+  }
+})
 </script>
 
 <template>
@@ -15,7 +42,7 @@ function onCreateNewEmployeeClick() {
       icon="/icons/file-white.svg"
       url="welcome-book"
     />
-    <div class="create">
+    <div class="create" :class="{ 'cannot-create': !canCreate }">
       <div class="background">
         <div class="h-white">
           <div class="title">
