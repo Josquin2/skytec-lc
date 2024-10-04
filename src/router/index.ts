43822adbd4c0783edf89b450472fb3bad9a38686
Murad Gapurovich <darkkukri0550@gmail.com>
2024-10-04 14:07:43 +0300

    Update index.ts

--- src/router/index.ts
+++ src/router/index.ts
@@ -37,7 +37,7 @@ const router = createRouter({
       component: () => import('@/views/VacanciesPage.vue')
     },
     {
-      path: '/about',
+      path: '/structure',
       name: 'about',
       component: AboutCompany
     },
